5687fd7c97065b2278a0e2ed6a358fae1604ec77
PedroLCamara <plcrlschool@gmail.com>
2021-05-12 18:14:46 -0300

    ExercicioVetores_Senai

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ExercicioForixiearai
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] embalagens=null;
+            Console.WriteLine("Há quantas embalagens de pão de queijo na prateleira?");
+            int quantidade_embalagens= int.Parse(Console.ReadLine());
+            embalagens= new int [(quantidade_embalagens)];
+            int num_embalagem= 1;
+            foreach (var item in embalagens)
+            {
+                Console.WriteLine($"Há quantos pães de queijo na {num_embalagem}ª embalagem?");
+                Console.ReadLine();
+                Console.WriteLine();
+                num_embalagem= num_embalagem+1;
+            }
+        }
+    }
+}
